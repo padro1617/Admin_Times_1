8c4854cb551170dc5274886d648b9d7f03bddf64
padro1617 <821807583@qq.com>
2015-02-11 08:55:56 +0800

    提交第一个版本

---
+++ MvcApplication/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication.Areas.Admin.Controllers
+{
+    public class AccountController : Controller
+    {
+        //用户注册
+        public ActionResult Register()
+        {
+            return View();
+        }
+        //用户登录
+        public ActionResult Login()
+        {
+            return View();
+        }
+        //退出
+        public ActionResult LogOut()
+        {
+            return View();
+        }
+        //密码重设
+        public ActionResult ResetPwd()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Admin/Account/
+
+        public ActionResult Index()
+        {            
+            return PartialView();
+        }
+
+
+        public ActionResult Edit()
+        {
+            return PartialView();
+        }
+
+    }
+}
---
+++ MvcApplication/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+         
+        // GET: /Admin/Home/
+        public ActionResult Index()
+        {           
+            return  PartialView();
+        }
+        //控制中心
+        public ActionResult Center()
+        {
+            return PartialView();
+        }
+        
+        //帮助文档
+        public ActionResult Help()
+        {
+            return PartialView();
+        }
+    }
+}
